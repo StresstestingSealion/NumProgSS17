645339b322c59517a14a3eb113510c4ebedc395f
Stresstesting_Sealion <sealion@xormail.de>
2017-06-17 11:23:41 +0200

    Additional bullshit

--- P8/P8/quadrature.c
+++ P8/P8/quadrature.c
@@ -7,16 +7,16 @@
 /*---------------------------------------------------------------*/
 
   #include <stdlib.h>
-  #include <assert.h>		
+  #include <assert.h>
   #include <math.h>
   #include "quadrature.h"
 
 #ifndef M_PI
 /* Copied from math.h  */
 #define M_PI		3.14159265358979323846
-#endif		
-  
-#define EPS 1.0e-10 
+#endif
+
+#define EPS 1.0e-10
  /* ------------------------------------------------------------
  * Constructor and destructor
  * ------------------------------------------------------------ */
@@ -30,7 +30,7 @@ new_quadrature(int m){
   quad->xq = (double *) calloc(m+1, sizeof(double));
   quad->w = (double *) calloc(m+1, sizeof(double));
   quad->m = m;
-  
+
   return quad;
 }
 
@@ -45,80 +45,62 @@ del_quadrature(pquadrature quad){
 
 pquadrature
 setup_midpointrule(void){
-	
-  /* ---------------------------------------------- */ 
-  /*                                                */
-  /* T T T T T     O O       D D           O O      */
-  /*     T        O   O      D   D        O   O     */
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T        O   O      D   D        O   O     */
-  /*     T         O O       D D           O O      */
-  /*                                                */ 
-  /* ---------------------------------------------- */
-	
+
+pquadrature midpoint = new_quadrature(2);
+return midpoint;
 }
 
 pquadrature
 setup_trapezoidalrule(void){
 
-  /* ---------------------------------------------- */ 
-  /*                                                */
-  /* T T T T T     O O       D D           O O      */
-  /*     T        O   O      D   D        O   O     */
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T        O   O      D   D        O   O     */
-  /*     T         O O       D D           O O      */
-  /*                                                */ 
-  /* ---------------------------------------------- */	
-		
+pquadrature trapezoid = new_quadrature(1);
+return trapezoid;
 }
 
 void
 map_quadrature_points(pquadrature quad, double a, double b, double *x){
 
-  /* ---------------------------------------------- */ 
+  /* ---------------------------------------------- */
   /*                                                */
   /* T T T T T     O O       D D           O O      */
   /*     T        O   O      D   D        O   O     */
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T       O     O     D     D     O     O    */ 
+  /*     T       O     O     D     D     O     O    */
+  /*     T       O     O     D     D     O     O    */
   /*     T        O   O      D   D        O   O     */
   /*     T         O O       D D           O O      */
-  /*                                                */ 
+  /*                                                */
   /* ---------------------------------------------- */
-  
+
 }
 
 double
 eval_quadrature(pquadrature quad, double a, double b, function f, void *data){
 
-  /* ---------------------------------------------- */ 
+  /* ---------------------------------------------- */
   /*                                                */
   /* T T T T T     O O       D D           O O      */
   /*     T        O   O      D   D        O   O     */
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T       O     O     D     D     O     O    */ 
+  /*     T       O     O     D     D     O     O    */
+  /*     T       O     O     D     D     O     O    */
   /*     T        O   O      D   D        O   O     */
   /*     T         O O       D D           O O      */
-  /*                                                */ 
+  /*                                                */
   /* ---------------------------------------------- */
- 
+
 }
 
 double
 eval_composite_quadrature(pquadrature quad, double a, double b, int n, function f, void *data){
 
-  /* ---------------------------------------------- */ 
+  /* ---------------------------------------------- */
   /*                                                */
   /* T T T T T     O O       D D           O O      */
   /*     T        O   O      D   D        O   O     */
-  /*     T       O     O     D     D     O     O    */ 
-  /*     T       O     O     D     D     O     O    */ 
+  /*     T       O     O     D     D     O     O    */
+  /*     T       O     O     D     D     O     O    */
   /*     T        O   O      D   D        O   O     */
   /*     T         O O       D D           O O      */
-  /*                                                */ 
-  /* ---------------------------------------------- */	
-  
+  /*                                                */
+  /* ---------------------------------------------- */
+
 }
