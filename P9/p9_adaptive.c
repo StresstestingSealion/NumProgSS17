563ea9c4f82a60e9c0c9c272fce9692c21f420a8
Stresstesting_Sealion <sealion@xormail.de>
2017-06-25 15:33:44 +0200

    Additional useless crap

---
+++ P9/p9_adaptive.c
@@ -0,0 +1,26 @@
+/*---------------------------------------------------------------*/
+/*		     Numerische Programmierung  	 	 */
+/* 		Serie 9 - Adaptive Quadratur 		 	 */
+/* ------------------------------------------------------------- */
+/*	Autoren: 		 				 */
+/*	Versionsnummer:						 */
+/*---------------------------------------------------------------*/
+
+		
+  #include <stdio.h>		
+  #include <stdlib.h> 
+  #include <math.h>
+  #include <assert.h>
+  
+  #include "quadrature.h"
+
+  /* ---------------------------------------------- */ 
+  /*                                                */
+  /* T T T T T     O O       D D           O O      */
+  /*     T        O   O      D   D        O   O     */
+  /*     T       O     O     D     D     O     O    */ 
+  /*     T       O     O     D     D     O     O    */ 
+  /*     T        O   O      D   D        O   O     */
+  /*     T         O O       D D           O O      */
+  /*                                                */ 
+  /* ---------------------------------------------- */ 
