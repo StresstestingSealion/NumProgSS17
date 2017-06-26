d0943b62488005f43b8be8fe77903d55fed1a20a
Nougat <schokolord@live.de>
2017-06-26 12:10:57 +0200

    bla bla bla

--- P8/P8/p8_quadrature.c
+++ P8/P8/p8_quadrature.c
@@ -7,16 +7,16 @@
 /*---------------------------------------------------------------*/
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <assert.h>
-#include <GL/freeglut.h>
-#include <GL/glut.h>
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <math.h>
+  #include <assert.h>
+  #include <GL/freeglut.h>
+  #include <GL/glut.h>
 
-#include "quadrature.h"
+  #include "quadrature.h"
 
-#ifndef M_PI
+  #ifndef M_PI
 /* Copied from math.h  */
 #define M_PI		3.14159265358979323846
 #endif
@@ -26,6 +26,7 @@
 * Global variable
 *-------------------------------------------------------------*/
 
+double e = 2.71828182845904523536;
 
 int window;
 int window_size_x = 500;
@@ -42,98 +43,100 @@ double precisision = 0.000001;
 * Example function
 *-------------------------------------------------------------*/
 
-
 double exampleFunction(double x, void *data) {
-    return (5 * exp(2 * x) * cos(x)) / (exp(M_PI) - 2);
+	x = ((pow(5 * e, 2 * x)) * cos(x)) / (pow(e, M_PI) -2);
+	return x/15; //compensate for ridiculous scaling
 }
 
 /* ------------------------------------------------------------
 * GLUT functions
 *-------------------------------------------------------------*/
 
-static void reshape(int width, int height) {
+static void reshape(int width, int height){
 
-    glViewport(0, 0, width, height);
-    glLoadIdentity();
-
-    if (width > height) {
-        glScalef((double) height / width, 1.0, 1.0);
-    } else {
-        glScalef(1.0, (double) width / height, 1.0);
-    }
+glViewport(0, 0, width, height); 	
+glLoadIdentity();
+	
+if(width > height){								
+	glScalef((double) height/width, 1.0, 1.0);
+}
+else{												
+	glScalef(1.0, (double) width/height, 1.0);
+}
 }
 
-static void createCoordinateSystem() {
+static void createCoordinateSystem(){
 
-    glColor3d(0.0, 0.0, 0.0);
-    glBegin(GL_LINES);
-    glVertex2f(-1.0, 0.0);
-    glVertex2f(1.0, 0.0);
+glColor3d(0.0, 0.0, 0.0);
+glBegin(GL_LINES);
+glVertex2f(-1.0, 0.0);
+glVertex2f(1.0, 0.0);
 
-    glVertex2f(0.0, 1.0);
-    glVertex2f(0.0, -1.0);
+glVertex2f(0.0, 1.0);
+glVertex2f(0.0, -1.0);
 
-    for (int i = 0; i <= 10; i++) {
+for(int i = 0; i <= 10; i++){
 
-        glVertex2f(1.0 / 10 * i, -0.05);
-        glVertex2f(1.0 / 10 * i, 0.05);
-        glVertex2f(-1.0 / 10 * i, -0.05);
-        glVertex2f(-1.0 / 10 * i, 0.05);
-        glVertex2f(-0.05, 1.0 / 10 * i);
-        glVertex2f(0.05, 1.0 / 10 * i);
-        glVertex2f(-0.05, -1.0 / 10 * i);
-        glVertex2f(0.05, -1.0 / 10 * i);
-    }
-
-    glEnd();
+	glVertex2f(1.0/10 * i, -0.05);
+	glVertex2f(1.0/10 * i, 0.05);
+	glVertex2f(-1.0/10 * i, -0.05);
+	glVertex2f(-1.0/10 * i, 0.05);
+	glVertex2f(-0.05, 1.0/10 * i);
+	glVertex2f(0.05, 1.0/10 * i);
+	glVertex2f(-0.05, -1.0/10 * i);
+	glVertex2f(0.05, -1.0/10 * i);
+}
+	
+glEnd();
 
 }
 
-static void display() {
+static void display(){
 
-    double x, x1, x2, sum;
-    int i;
-    double a = 0;
-    double b = M_PI / 2;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+double x, x1, x2, sum;
+int i;
+double a = 0;
+double b = M_PI / 2;
+glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	
 
-    createCoordinateSystem();
+createCoordinateSystem();
+	
+glClearColor(1.0,1.0,1.0,1.0);
 
-    glClearColor(1.0, 1.0, 1.0, 1.0);
+glPointSize(2.0);
+glBegin(GL_LINE_STRIP);
 
-    glPointSize(2.0);
-    glBegin(GL_LINE_STRIP);
+for(x = 0.0; x <= M_PI / 2; x += precisision){
+	glColor3f(0.8, 0.0, 0.0);
+	glVertex2f(x / 2, exampleFunction(x, data));
+}
 
-    for (x = 0.0; x <= M_PI / 2; x += precisision) {
-        glColor3f(0.8, 0.0, 0.0);
-        glVertex2f(x / 2, exampleFunction(x, data));
-    }
+glEnd();
+glBegin(GL_LINE_STRIP);
+glColor3f(0.0, 0.8, 0.0);
 
-    glEnd();
-    glBegin(GL_LINE_STRIP);
-    glColor3f(0.0, 0.8, 0.0);
+for(i = 0; i < interpolationPoints; i++){
+	x1 = a + (b - a) / interpolationPoints * i;
+	x2 = a + (b - a) / interpolationPoints * (i + 1);
+	glVertex2f(x1 / 2, 0);
 
-    for (i = 0; i < interpolationPoints; i++) {
-        x1 = a + (b - a) / interpolationPoints * i;
-        x2 = a + (b - a) / interpolationPoints * (i + 1);
-        glVertex2f(x1 / 2, 0);
+	if(quad->m == 0){
+		glVertex2f(x1 / 2, exampleFunction((x1 + x2) / 2, data));
+		glVertex2f(x2 / 2, exampleFunction((x1 + x2) / 2, data));
+	}
+	else{
+		glVertex2f(x1 / 2, exampleFunction(x1, data));
+		glVertex2f(x2 / 2, exampleFunction(x2, data));
+	}
+	glVertex2f(x2 / 2, 0);
+}
+glEnd();
 
-        if (quad->m == 0) {
-            glVertex2f(x1 / 2, exampleFunction((x1 + x2) / 2, data));
-            glVertex2f(x2 / 2, exampleFunction((x1 + x2) / 2, data));
-        } else {
-            glVertex2f(x1 / 2, exampleFunction(x1, data));
-            glVertex2f(x2 / 2, exampleFunction(x2, data));
-        }
-        glVertex2f(x2 / 2, 0);
-    }
-    glEnd();
+sum = eval_composite_quadrature(quad, 0, M_PI/2, interpolationPoints, &exampleFunction, data);
+printf("Integrated area: %f\n", sum*15);	//more scaling
 
-    sum = eval_composite_quadrature(quad, 0, M_PI / 2, interpolationPoints, &exampleFunction, data);
-    printf("Integrated area: %f\n", sum);
-
-    glFlush();
-
+glFlush();
+  
 }
 
 /* Controls: 
@@ -143,51 +146,47 @@ static void display() {
 	2: toggle trapezoid rule
 */
 static void
-key(unsigned char key, int x, int y) {
-    (void) x;
-    (void) y;
-
-    switch (key) {
-        case 'a':
-            interpolationPoints -= 1;
-            glutPostRedisplay();
-            break;
-        case 'q':
-            interpolationPoints += 1;
-            glutPostRedisplay();
-            break;
-        case '1':
-            quad = setup_midpointrule();
-            glutPostRedisplay();
-            break;
-        case '2':
-            quad = setup_trapezoidalrule();
-            glutPostRedisplay();
-            break;
-        case 27 :
-            exit(EXIT_SUCCESS);
-            break;
-        default :
-            break;
-    }
+key(unsigned char key, int x, int y){
+(void) x;
+(void) y;
+	
+switch (key){
+  case 'a':		interpolationPoints -= 1;
+			glutPostRedisplay();
+			break;
+  case 'q':		interpolationPoints += 1;
+			glutPostRedisplay();
+			break;
+  case '1':		quad = setup_midpointrule();
+			glutPostRedisplay();
+			break;
+  case '2':		quad = setup_trapezoidalrule();
+			glutPostRedisplay();
+			break;
+  case 27 :
+			exit (EXIT_SUCCESS);
+			break;
+  default :
+			break;	 		
+  }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv){
 
-    data[0] = 0.0;
-    data[1] = 1.0;
+data[0] = 0.0;
+data[1] = 1.0;
+	
+quad = setup_midpointrule();
 
-    quad = setup_midpointrule();
+glutInit(&argc, argv);
 
-    glutInit(&argc, argv);
+window = glutCreateWindow ("Quadrature");
+glutPositionWindow(150, 150);
+glutReshapeWindow(window_size_x, window_size_y);
+glutDisplayFunc(display);
+glutReshapeFunc(reshape);
+glutKeyboardFunc(key);
 
-    window = glutCreateWindow("Quadrature");
-    glutPositionWindow(150, 150);
-    glutReshapeWindow(window_size_x, window_size_y);
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutKeyboardFunc(key);
-
-    glutMainLoop();
-    return 0;
+glutMainLoop();
+return 0;
 }
---
+++ P8/quadrature.c
@@ -0,0 +1,113 @@
+/*---------------------------------------------------------------*/
+/*		     Numerische Programmierung  	 	 */
+/* 	Serie 8 - Approximation von Integralen Quadratur  	 */
+/* ------------------------------------------------------------- */
+/*	Autoren: 				 */
+/*	Versionsnummer:						 */
+/*---------------------------------------------------------------*/
+
+#include <stdlib.h>
+#include <assert.h>
+#include <math.h>
+#include "quadrature.h"
+
+#ifndef M_PI
+/* Copied from math.h  */
+#define M_PI		3.14159265358979323846
+#endif
+
+#define EPS 1.0e-10
+
+/* ------------------------------------------------------------
+* Constructor and destructor
+* ------------------------------------------------------------ */
+
+pquadrature
+new_quadrature(int m) {
+
+    pquadrature quad;
+
+    quad = (pquadrature) calloc(1, sizeof(quadrature));
+    quad->xq = (double *) calloc(m + 1, sizeof(double));
+    quad->w = (double *) calloc(m + 1, sizeof(double));
+    quad->m = m;
+
+    return quad;
+}
+
+void
+del_quadrature(pquadrature quad) {
+
+    free(quad->xq);
+    free(quad->w);
+    free(quad);
+
+}
+
+pquadrature
+setup_midpointrule(void) {
+
+    pquadrature midpoint = new_quadrature(0);
+
+    midpoint->xq[0] = 0;
+    midpoint->w[0] = 2;
+
+    return midpoint;
+}
+
+pquadrature
+setup_trapezoidalrule(void) {
+
+    pquadrature trapezoid = new_quadrature(1);
+
+    trapezoid->xq[0] = -1;
+    trapezoid->xq[1] = 1;
+    trapezoid->w[0] = 1;
+    trapezoid->w[1] = 1;
+
+    return trapezoid;
+}
+
+void
+map_quadrature_points(pquadrature quad, double a, double b, double *x) {
+
+    int i;
+    int m = quad->m;
+
+    for (i = 0; i <= m; i++) {
+        x[i] = (b + a) / 2 + (b - a) / 2 * quad->xq[i];
+    }
+}
+
+double
+eval_quadrature(pquadrature quad, double a, double b, function f, void *data) {
+
+    double sum = 0.0;
+    int m = quad->m;
+    int i;
+    double *x = malloc((m + 1) * sizeof(double));
+    map_quadrature_points(quad, a, b, x);
+
+    for (i = 0; i <= m; i++) {
+        sum += quad->w[i] * f(x[i], data);
+    }
+
+    free(x);
+
+    sum *= ((b - a) / 2);
+    return sum;
+}
+
+double
+eval_composite_quadrature(pquadrature quad, double a, double b, int n, function f, void *data) {
+
+    int i;
+    double sum = 0.0;
+    double distance = (b - a) / n;
+
+    for (i = 0; i < n; i++) {
+        sum += eval_quadrature(quad, a + distance * i, a + distance * (i + 1), f, data);
+    }
+
+    return sum;
+}
