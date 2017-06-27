641121954a9439bc2bd47ed9a9eee958ebecb1e7
Stresstesting_Sealion <sealion@xormail.de>
2017-06-27 18:54:37 +0200

    mystery solution

--- P9/basic.c
+++ P9/basic.c
@@ -4,61 +4,64 @@
 #include <stdlib.h>
 
 #ifdef __WIN32__
-
 #  include <Windows.h>
 #  include <MMSystem.h>
 
-struct _stopwatch {
+  struct _stopwatch {
     DWORD start;
     DWORD current;
-};
+  };
 #else
 #  include <sys/times.h>
 #  include <unistd.h>
-struct _stopwatch {
-  struct tms start;
-  struct tms current;
-  real clk_tck;
-};
+  struct _stopwatch {
+    struct tms start;
+    struct tms current;
+    real clk_tck;
+  };
 #endif
 
 pstopwatch
-new_stopwatch() {
-    pstopwatch sw;
+new_stopwatch()
+{
+  pstopwatch sw;
 
-    sw = (pstopwatch) malloc(sizeof(stopwatch));
-
+  sw = (pstopwatch) malloc(sizeof(stopwatch));
+  
 #ifndef WIN32
-    sw->clk_tck = sysconf(_SC_CLK_TCK);
+  sw->clk_tck = sysconf(_SC_CLK_TCK);
 #endif
 
-    return sw;
+  return sw;
 }
 
 void
-del_stopwatch(pstopwatch sw) {
-    free(sw);
+del_stopwatch(pstopwatch sw)
+{
+  free(sw);
 }
 
 void
-start_stopwatch(pstopwatch sw) {
+start_stopwatch(pstopwatch sw)
+{
 #ifdef WIN32
-    sw->start = timeGetTime();
+  sw->start = timeGetTime();
 #else
-    times(&sw->start);
+  times(&sw->start);
 #endif
 }
 
 real
-stop_stopwatch(pstopwatch sw) {
+stop_stopwatch(pstopwatch sw)
+{
 #ifdef WIN32
-    sw->current = timeGetTime();
+  sw->current = timeGetTime();
 
-    return (sw->current - sw->start) * 0.001;
+  return (sw->current - sw->start) * 0.001;
 #else
-    times(&sw->current);
+  times(&sw->current);
 
-    return (sw->current.tms_utime - sw->start.tms_utime
-        + sw->current.tms_stime - sw->start.tms_stime) / sw->clk_tck;
+  return (sw->current.tms_utime - sw->start.tms_utime
+	  + sw->current.tms_stime - sw->start.tms_stime) / sw->clk_tck;
 #endif
 }
--- P9/quadrature.h
+++ P9/quadrature.h
@@ -1,28 +1,27 @@
 
-#ifndef QUADRATURE_H
-#define QUADRATURE_H
+  #ifndef QUADRATURE_H
+  #define QUADRATURE_H
+  
+  #include "basic.h"
 
-#include "basic.h"
+  /* prototyp function*/
+  typedef double (*function)(double x, void *data);
 
-/* prototyp function*/
-typedef double (*function)(double x, void *data);
+  typedef struct _quadrature quadrature;
 
-typedef struct _quadrature quadrature;
+  typedef quadrature *pquadrature;
+  
+ 
+  struct _quadrature {
+    int m;				/* quadrature order */
 
-typedef quadrature *pquadrature;
+    double *xq;			/* quadrature points */
+    double *w;			/* quadrature weights */
+  };
 
-
-struct _quadrature {
-    int m;                /* quadrature order */
-
-    double *xq;            /* quadrature points */
-    double *w;            /* quadrature weights */
-};
-
-
-/* ------------------------------------------------------------
-* Constructor and destructor
-* ------------------------------------------------------------ */
+ /* ------------------------------------------------------------
+ * Constructor and destructor
+ * ------------------------------------------------------------ */
 
 pquadrature
 new_quadrature(int m);
@@ -30,9 +29,9 @@ new_quadrature(int m);
 void
 del_quadrature(pquadrature quad);
 
-/* ------------------------------------------------------------
-* Set up midpoint rule and trapezoidal rule
-* ------------------------------------------------------------ */
+ /* ------------------------------------------------------------
+ * Set up midpoint rule and trapezoidal rule
+ * ------------------------------------------------------------ */
 
 /* Allocate memory and set up quadrature weights and points on [-1,1] */
 
@@ -48,19 +47,19 @@ void
 map_quadrature_points(pquadrature quad, double a, double b, double *x);
 
 
-/* ------------------------------------------------------------
-* Compute quadrature formulas  composite
-* ------------------------------------------------------------ */
+ /* ------------------------------------------------------------
+ * Compute quadrature formulas  composite 
+ * ------------------------------------------------------------ */
 
-/* Compute integral of f on [a,b] with given quadrature formula */
-
+/* Compute integral of f on [a,b] with given quadrature formula */ 
+ 
 double
 eval_quadrature(pquadrature quad, double a, double b, function f, void *data);
 
 /* Compute integral of f on [a,b] with given composite quadrature formula 
-   using n even subintervalls of [a,b] */
+   using n even subintervalls of [a,b] */ 
 
 double
-eval_composite_quadrature(pquadrature quad, double a, double b, int n, function f, void *data);
+eval_composite_quadrature(pquadrature quad_m, pquadrature quad_t, double a, double b, double rel_error, function f, void *data, int *steps);
 
-#endif
+  #endif
